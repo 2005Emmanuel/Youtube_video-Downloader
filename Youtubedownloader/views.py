073f38dc0439f5bd7957d977fac056e3c9390568
Your Name <you@example.com>
2023-01-16 23:27:03 +0100

    made a change

--- Youtubedownloader/views.py
+++ Youtubedownloader/views.py
@@ -44,7 +44,7 @@ def download_page(request): #view to get the resolution and url of the page
 	
 	title = yt.title
 	author = yt.author
-	length = str(yt.length//60) + ' minutes'
+	length = str(yt.length) + ' minutes'
 	if length == 0:
 		length = str(yt.length) + ' seconds'
 
@@ -77,12 +77,12 @@ def success(request, res): #view to download the video it is done with file hand
 	size = streams.filter(res=res).first().filesize // 1048576
 	print(size)
 	if request.method == 'POST' and size < 900:
-		streams.filter(res=res).first().download(output_path = dirs, filename = "{title}.mp4")
-		file = FileWrapper(open(f'{dirs}/{title}.mp4', 'rb'))
+		streams.filter(res=res).first().download(output_path = dirs, filename = "video.mp4")
+		file = FileWrapper(open(f'{dirs}/video.mp4', 'rb'))
 		# path =  '/home/runner/youtube-video-downloader/downloads/video' + '.mp4'
 		# o = dirs + title + '.mp4'
-		response = HttpResponse(file, content_type = 'video/mp4') #vnd.mp4
-		response['Content-Disposition'] = 'attachment; filename = "{title}.mp4"'
+		response = HttpResponse(file, content_type = 'vnd.mp4') #vnd.mp4
+		response['Content-Disposition'] = 'attachment; filename = "video.mp4"'
 		# os.remove(f'{dirs}/video.mp4')
 		return response
 	else:
